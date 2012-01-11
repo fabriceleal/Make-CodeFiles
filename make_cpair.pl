2dd72c649d99040d2cab6e2b3059d7851ef668ed
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-01-11 12:48:33 -0800

    with mode and some validations

--- make_cpair.pl
+++ make_cpair.pl
@@ -2,23 +2,36 @@
 
 # Generates C files .h and .c
 
-$fname = $ARGV[0];
+my($fname, @mode) = $ARGV[0], undef;
 
-# TODO: Create var for using as the #define macro. Remove white spaces, upper case all.
+# Remove all white spaces here ...
+$fname =~ s/\s/_/;
 
-# TODO: Mode: .c+.h, .h, .c
+# Create var for using as the #define macro. Remove white spaces, upper case all.
+$fname_macro = $fname;
+$fname_macro =~ tr/[a-z]/[A-Z]/;
+
+# Mode: ch, h, c
+{
+	my(@mode_tmp) = undef;
+	@mode_tmp = split('', $ARGV[1]);
+	foreach(@mode_tmp){
+		$mode{$_} = 1;
+	}
+}
+
 
 # If files exist, throw error ...
-(-e "$fname.c") and die ("File $fname.c already exists!");
-(-e "$fname.h") and die ("File $fname.h already exists!");
+($mode{"c"}) and (-e "$fname.c") and die ("File $fname.c already exists!");
+($mode{"h"}) and (-e "$fname.h") and die ("File $fname.h already exists!");
 
 # Create and open files (.c and .h)
-open (fs_c, ">$fname.c") or die ("Could not generate file $fname.c, $!");
-open (fs_h, ">$fname.h") or die ("Could not generate file $fname.h, $!");
+($mode{"c"}) and (open (fs_c, ">$fname.c") or die ("Could not generate file $fname.c, $!"));
+($mode{"h"}) and (open (fs_h, ">$fname.h") or die ("Could not generate file $fname.h, $!"));
 
 #  Print code templates
 # The .c file
-$file_c = 
+($mode{"c"}) and $file_c = 
 "/**
  * \@file $fname.c
  * \@author Fabrice Leal <fabrice [dot]leal[dot]ch[at]gmail[dot]com>
@@ -33,13 +46,13 @@ $file_c =
  *
  */
 
-#include "$fname.h"
+#include \"$fname.h\"
 
 
 ";
 
 # The .h file
-$file_h = 
+($mode{"h"}) and $file_h = 
 "/**
  * \@file $fname.h
  * \@author Fabrice Leal <fabrice [dot]leal[dot]ch[at]gmail[dot]com>
@@ -57,22 +70,22 @@ $file_h =
 
 // VAMI stands for Validation Against Multiple Includes :)
 
-#ifndef ___VAMI_$fname
-#define ___VAMI_$fname
+#ifndef ___VAMI_$fname_macro
+#define ___VAMI_$fname_macro
 
 // A header file only declares the functions headers and structs used. 
 // Implementations go into the .c file ;)
 
 
-#endif // ___VAMI_$fname
+#endif // ___VAMI_$fname_macro
 ";
 
 # Prints to files ...
-print fs_c $file_c ;
-print fs_h $file_h ;
+($mode{"c"}) and print fs_c $file_c ;
+($mode{"h"}) and print fs_h $file_h ;
 
 # Close files 
-close fs_c ;
-close fs_h ;
+($mode{"c"}) and close fs_c ;
+($mode{"h"}) and close fs_h ;
 
 
